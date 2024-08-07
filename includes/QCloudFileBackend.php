d9ab3d6f70d8f12f541483cdd9d97dc6ba3ed736
RazeSoldier <razesoldier@outlook.com>
2024-08-07 16:06:18 +0800

    fix(#2): Overriding FileBackendStore::doMoveInternal()
    Because in Core 01b2db92, the method changed to abstract

--- includes/QCloudFileBackend.php
+++ includes/QCloudFileBackend.php
@@ -437,4 +437,20 @@ class QCloudFileBackend extends \FileBackendStore {
 	public function getViewpoint(): string {
 		return $this->viewpoint;
 	}
+
+	protected function doMoveInternal(array $params)
+	{
+		unset( $params['async'] ); // two steps, won't work here :)
+		$nsrc = FileBackend::normalizeStoragePath( $params['src'] );
+		$ndst = FileBackend::normalizeStoragePath( $params['dst'] );
+		// Copy source to dest
+		$status = $this->copyInternal( $params );
+		if ( $nsrc !== $ndst && $status->isOK() ) {
+			// Delete source (only fails due to races or network problems)
+			$status->merge( $this->deleteInternal( [ 'src' => $params['src'] ] ) );
+			$status->setResult( true, $status->value ); // ignore delete() errors
+		}
+
+		return $status;
+	}
 }
